b0af4d68749e67b5846324274c7e723792583ca5
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-06-06 17:59:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
@@ -11,6 +11,7 @@ setup(
     version='1.0',
     description='An auto downloader and uploader for TikTok videos.',
     long_description=long_description,
+	long_description_content_type='text/x-rst',
     url='https://github.com/Coloradohusky/TikUp',
     author='Coloradohusky',
     py_modules=['tikup'],
