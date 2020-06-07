cd7e5af311b9875777091145c27eb055ffbfe1d1
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-06-06 21:21:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,12 @@ setup(
     long_description_content_type='text/x-rst',
     url='https://github.com/Coloradohusky/TikUp',
     author='Coloradohusky',
-    py_modules=['tikup'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'tikup = tikup.tikup:main',
+        ],
+    },
     python_requires='>=3.5, <4',
     install_requires=['internetarchive>=1.9.3', 'TikTokApi>=3.1.3', 'youtube-dl>=2020.06.06']
 )
